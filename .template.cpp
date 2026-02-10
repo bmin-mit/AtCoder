e2319b48ec02b8381c510df1e4bb8c093d4ab534
Binh Minh <bmmit.work@gmail.com>
2026-02-10 13:36:15 +0700

    dp D - Knapsack 1

---
+++ .template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void solve()
+{
+}
+
+signed main()
+{
+    FAST_IO;
+
+    // MULTI
+    solve();
+
+    return 0;
+}
