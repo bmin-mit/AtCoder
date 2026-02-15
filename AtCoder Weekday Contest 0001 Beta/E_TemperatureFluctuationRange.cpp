1a3810059271e0e18050a47727434fc04c85dbae
Binh Minh <bmmit.work@gmail.com>
2026-02-15 16:06:54 +0700

    AtCoder Weekday Contest 0001 Beta - E - Temperature Fluctuation Range

---
+++ AtCoder Weekday Contest 0001 Beta/E_TemperatureFluctuationRange.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), (void)cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p);
+
+#define P(T, O, C)                          \
+    template <class U>                      \
+    ostream &operator<<(ostream &o, T<U> v) \
+    {                                       \
+        o << O;                             \
+        int f = 0;                          \
+        for (auto &i : v)                   \
+            o << (f++ ? ", " : "") << i;    \
+        return o << C;                      \
+    }
+
+#define Q(T, O, C)                             \
+    template <class K, class V>                \
+    ostream &operator<<(ostream &o, T<K, V> v) \
+    {                                          \
+        o << O;                                \
+        int f = 0;                             \
+        for (auto &i : v)                      \
+            o << (f++ ? ", " : "") << i;       \
+        return o << C;                         \
+    }
+
+P(vector, '[', ']')
+P(set, '{', '}')
+Q(map, '{', '}')
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+
+template <class T, size_t N>
+ostream &operator<<(ostream &o, array<T, N> v)
+{
+    o << "[";
+    int f = 0;
+    for (auto &i : v)
+        o << (f++ ? ", " : "") << i;
+    return o << "]";
+}
+
+void dbg(const char *s) { cerr << endl; }
+
+template <class T, class... A>
+void dbg(const char *s, T v, A... a)
+{
+    const char *c = s;
+    int b = 0;
+    while (*c && (*c != ',' || b))
+        b += bool(strchr("({[", *c)), b -= bool(strchr(")}]", *c)), c++;
+    cerr.write(s, c - s) << " = " << v << (sizeof...(a) ? " |" : "");
+    if (sizeof...(a))
+        dbg(c + 1, a...);
+    else
+        cerr << endl;
+}
+
+#ifdef LOCAL
+#define debug(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+    for (int &elem : arr)
+        cin >> elem;
+
+    deque<int> maxDq, minDq;
+    maxDq.push_back(0), minDq.push_back(0);
+
+    int res = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        while (!maxDq.empty() && arr[maxDq.back()] <= arr[i])
+            maxDq.pop_back();
+        maxDq.push_back(i);
+
+        while (!minDq.empty() && arr[minDq.back()] >= arr[i])
+            minDq.pop_back();
+        minDq.push_back(i);
+
+        if (minDq.front() <= i - k)
+            minDq.pop_front();
+        if (maxDq.front() <= i - k)
+            maxDq.pop_front();
+
+        res = max(res, arr[maxDq.front()] - arr[minDq.front()]);
+    }
+
+    cout << res;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    // MULTI
+    solve();
+
+    return 0;
+}
