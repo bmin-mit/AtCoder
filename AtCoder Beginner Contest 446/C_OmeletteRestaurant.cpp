6b4264c5cbbf74dde7d00143b99d19908abe46ba
Binh Minh <bmmit.work@gmail.com>
2026-02-21 20:45:17 +0700

    AtCoder Beginner Contest 446 - ABCD

---
+++ AtCoder Beginner Contest 446/C_OmeletteRestaurant.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), (void)cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p);
+
+#define P(T, O, C)                          \
+    template <class U>                      \
+    ostream &operator<<(ostream &o, T<U> v) \
+    {                                       \
+        o << O;                             \
+        int f = 0;                          \
+        for (auto &i : v)                   \
+            o << (f++ ? ", " : "") << i;    \
+        return o << C;                      \
+    }
+
+#define Q(T, O, C)                             \
+    template <class K, class V>                \
+    ostream &operator<<(ostream &o, T<K, V> v) \
+    {                                          \
+        o << O;                                \
+        int f = 0;                             \
+        for (auto &i : v)                      \
+            o << (f++ ? ", " : "") << i;       \
+        return o << C;                         \
+    }
+
+P(vector, '[', ']')
+P(set, '{', '}')
+Q(map, '{', '}')
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+
+template <class T, size_t N>
+ostream &operator<<(ostream &o, array<T, N> v)
+{
+    o << "[";
+    int f = 0;
+    for (auto &i : v)
+        o << (f++ ? ", " : "") << i;
+    return o << "]";
+}
+
+void dbg(const char *s) { cerr << endl; }
+
+template <class T, class... A>
+void dbg(const char *s, T v, A... a)
+{
+    const char *c = s;
+    int b = 0;
+    while (*c && (*c != ',' || b))
+        b += bool(strchr("({[", *c)), b -= bool(strchr(")}]", *c)), c++;
+    cerr.write(s, c - s) << " = " << v << (sizeof...(a) ? " |" : "");
+    if (sizeof...(a))
+        dbg(c + 1, a...);
+    else
+        cerr << endl;
+}
+
+#ifdef LOCAL
+#define debug(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void solve()
+{
+    int n, d;
+    cin >> n >> d;
+
+    vector<int> buy(n + 1), use(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> buy[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> use[i];
+    }
+
+    deque<pair<int, int>> q;
+    for (int i = 1; i <= n; i++)
+    {
+        q.push_back({buy[i], i});
+
+        while (use[i])
+        {
+            int val = min(q.front().first, use[i]);
+
+            q.front().first -= val;
+            use[i] -= val;
+            if (q.front().first == 0)
+                q.pop_front();
+            else
+                break;
+        }
+
+        while (!q.empty() && q.front().second <= i - d)
+        {
+            q.pop_front();
+        }
+    }
+
+    int res = 0;
+    for (auto &[val, idx] : q)
+        res += val;
+    cout << res << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    MULTI
+    solve();
+
+    return 0;
+}
